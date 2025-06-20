8320eae792d9b4a7cc8a9474c02040d14f58b437
Suganyadoodleblue <suganya26.doodleblue@gmail.com>
2025-06-20 21:52:43 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'todo.dart';
+import 'todo_item.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Todo List',
+      debugShowCheckedModeBanner: false,
+      home: const TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({super.key});
+
+  @override
+  State<TodoListScreen> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final List<Todo> _todos = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addTodo() {
+    if (_controller.text.trim().isEmpty) return;
+    setState(() {
+      _todos.add(Todo(title: _controller.text.trim()));
+      _controller.clear();
+    });
+  }
+
+  void _toggleTodo(int index) {
+    setState(() {
+      _todos[index].isDone = !_todos[index].isDone;
+    });
+  }
+
+  void _deleteTodo(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todo List')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter a task',
+                    ),
+                    onSubmitted: (_) => _addTodo(),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: _addTodo,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                return TodoItem(
+                  todo: _todos[index],
+                  onToggle: () => _toggleTodo(index),
+                  onDelete: () => _deleteTodo(index),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
